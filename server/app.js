1446d2f185a9fa35764c37e279e812fca2f49331
BKBK <lidamao233@163.com>
2022-07-29 18:03:51 -0700

    test merge2

--- server/app.js
+++ server/app.js
@@ -13,7 +13,6 @@ var authRouter = require('./routes/auth')
 var app = express();
 
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
