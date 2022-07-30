0e3e2c7b6a95d1e6fa2b35b9d939e9328b6ae348
etherealgloriaw <gloriawithwang@gmail.com>
2022-07-30 04:06:28 -0700

    888

--- client/src/redux/auth/services.js
+++ client/src/redux/auth/services.js
@@ -36,7 +36,7 @@ const signUp = async(form) => {
 
 const editProfile = async(edited) =>{
     // const id = edited._id;
-    const response = await fetch(`/${url}/${id}`, {
+    const response = await fetch(url + "/Edit", {
         method: 'PATCH',
         headers: {
             "Content-type": "application/json",
